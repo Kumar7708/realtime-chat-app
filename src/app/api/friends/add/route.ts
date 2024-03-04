652efaaea5734c45373b7c16075be38fbfcec9d8
Bavara Kumar Swami <kumarbevara7708@gmail.com>
2024-03-05 02:20:34 +0530

    Final

---
+++ src/app/api/friends/add/route.ts
@@ -0,0 +1,61 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+        const { email: emailToAdd } = addFriendValidator.parse(body.email);
+
+        const idToAdd = await fetchRedis("get", `user:email:${emailToAdd}`)
+
+        if (!idToAdd) {
+            return new Response("This person does not exists", { status: 400 });
+        }
+
+        const session = await getServerSession(authOptions);
+
+        if (!session) {
+            return new Response("Unauthorized", { status: 401 });
+        }
+
+        if (idToAdd === session.user.id) {
+            return new Response("You cannot add yourself as a friend", { status: 400 })
+        }
+
+        const isArleadyAdded = (await fetchRedis('sismember', `user:${idToAdd}:incoming_friend_requests`, session.user.id)) as 0 | 1
+
+        if (isArleadyAdded) {
+            return new Response("Already added this user", { status: 400 });
+        }
+
+        const isArleadyFriend = (await fetchRedis('sismember', `user:${session.user.id}:friends`, idToAdd)) as 0 | 1
+
+        if (isArleadyFriend) {
+            return new Response("Already friends with this user", { status: 400 });
+        }
+        pusherServer.trigger(
+            toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+            'incoming_friend_requests',
+            {
+                senderId: session.user.id,
+                senderEmail: session.user.email
+            })
+
+        db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
+
+        return new Response("ok")
+
+
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return new Response("Invalid request payload", { status: 422 });
+        }
+        return new Response("Invalid request", { status: 400 });
+    }
+}
---
+++ src/types/db.d.ts
@@ -0,0 +1,24 @@
+interface User {
+    name: string,
+    email: string,
+    id: string,
+    image: string
+}
+
+interface Chat {
+    id: string,
+    messages: Message[]
+}
+
+interface Message {
+    id: string,
+    senderId: string,
+    text: string,
+    timestamp: number
+}
+
+interface FriendRequest {
+    id: string,
+    senderId: string,
+    receiverId: string
+}
